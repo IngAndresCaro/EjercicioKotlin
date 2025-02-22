4e2a77983172552a66c15dfc22d52505ff1b8312
Andres Caro <andrcagl@suramericana.com.co>
2025-02-22 16:54:42 -0500

    Kotlin y sus tipos de variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,4 +16,29 @@ fun main() {
 
     println(Pi)
 
+    val boolean = true;
+    val numeroLargo: Long = 3000000000L;
+    val numeroDecimal: Double = 3.1416;
+    val numeroDecimal2: Float = 3.1416f;
+    val numeroHexadecimal: Int = 0x0F;
+    val numeroBinario: Int = 0b00001011;
+
+    val primerValor = 10;
+    primerValor.toDouble();
+    primerValor.toFloat();
+    primerValor.toLong();
+    primerValor.toShort();
+
+    val segundoValor = 25;
+
+    val tercerValor =primerValor.minus(segundoValor) //minus resta es lo mismo que primerValor - segundoValor
+    println(tercerValor)
+
+    val cuartoValor = primerValor.plus(segundoValor) //plus suma es lo mismo que primerValor + segundoValor
+
+    val apellito = "Caro"
+    val name = "Andres"
+
+    val nombreCompleto = "Mi nombre es $name y mi apellido es $apellito"
+    println(nombreCompleto)
 }
