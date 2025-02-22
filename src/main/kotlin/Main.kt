ed28ab7e23fcd3fa7561641bce9f0bcc15cd85a6
Andres Caro <andrcagl@suramericana.com.co>
2025-02-22 16:42:01 -0500

    Se realiza entendimiento de manejo de variables en kotlin donde var es para un valor cambiable en el tiempo, val es solo para lectura y const no cambia durante su ejecución por esto se declara porfuera de la funcion

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,19 @@
 package org.example
 
+const val Pi = 3.1416;//valor que no cambia en tiempo de ejecución
+
 fun main() {
     println("Hello World!")
+    var dinero = 10;
+    println(dinero);
+
+    dinero = 5;
+    println(dinero);
+
+    val nombre = "Juan";
+    println(nombre);
+    // nombre = "Pedro"; // Error variable de solo lectura no es posible reasignar
+
+    println(Pi)
+
 }
