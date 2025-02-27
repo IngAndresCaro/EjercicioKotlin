9140adaf1263b9c174337b9d11b93c2e2e73ee14
andresCaro <andrescaro189@gmail.com>
2025-02-27 18:42:34 -0500

    Se realiza entendimiento de dowhile y while generando las condiciones para los ciclos

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,35 +1,24 @@
 package org.example
 
 fun main() {
-    val nombreColor = "Amarillo"
-
-    when (nombreColor) {
-        "Amarillo" -> {
-            println("El amarillo es el color del sol")
-        }
-        "Azul" -> println("El azul es el color del cielo")
-        "Rojo","Carmesi" -> println("El rojo es el color de la pasion")
-
-        else -> println("No tengo información sobre el color")
+    var contador = 10
+    //mientras contador sea mayor a 0
+    while (contador > 0) {
+        println("Contador while: $contador")
+        contador--
     }
+    //hacer mientras contador sea mayor a 0
+    contador = 10
+    do {
+        println("Contador doWhile: $contador")
+        contador--
+    } while (contador > 0)
 
-    val code = 200
-    when (code) {
-        in 200..299 -> println("Todo ha ido bien")
-        in 300..399 -> println("Redirección")
-        in 400..499 -> println("Error del cliente")
-        in 500..599 -> println("Error del servidor")
-        else -> println("Código desconocido")
-    }
-
-    val tallaDeZapatos = 41
-    val mensaje = when (tallaDeZapatos) {
-        41, 42 -> "Tenemos disponible"
-        44, 45 -> "Casi no tenemos!"
-        46 -> "No tenemos disponible"
-        else -> "No tenemos disponible"
-    }
-    println(mensaje)
-
+    //ejerccio 1
+    do{
+        println("Generando numero aleatorio")
+        val numeroAleatorio = (0..100).random()
+        println(" El numero generado es: $numeroAleatorio")
+    } while (numeroAleatorio > 50)
 
 }
