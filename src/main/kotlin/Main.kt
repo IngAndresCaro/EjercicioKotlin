182e5325bbc424b034cb5765805243cd678dad63
andresCaro <andrescaro189@gmail.com>
2025-02-28 18:58:49 -0500

    Se realiza prueba de concepto

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,4 +2,16 @@ package org.example
 
 fun main() {
 
+    var nombre: String? = null
+
+    try {
+        nombre?.length  // No forzar una excepción
+        nombre!!.length // Forzar una excepción esto saca el error
+
+    } catch (e: NullPointerException) {
+        println("Error")
+    }finally {
+        println("Continua sin importar que")
+    }
+
 }
