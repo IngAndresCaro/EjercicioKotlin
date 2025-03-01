8877814800e4825bcf7f6ab5c193e8013dfc2054
andresCaro <andrescaro189@gmail.com>
2025-03-01 12:30:12 -0500

    Se realiza entendimiento de las listas vs array

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,34 @@
 package org.example
 
 fun main() {
-    var nombre: String? = null
-    val caracteresDeNombre: Int = nombre?.length ?: 0 // Elvis operator
-    println(caracteresDeNombre)
+    //Lista no mutable
+    val listDeNombres = listOf<String>("Juan", "Pedro", "Luis", "Carlos", "Juan", "Pedro", "Luis", "Carlos")
+    println(listDeNombres)
+    //lista mutable
+    val listaVasia = mutableListOf<String>()
+    println(listaVasia)
+    listaVasia.add("Mateo")
+    println(listaVasia)
 
+    val valorUsandoGet = listaVasia.get(0)
+    println(valorUsandoGet)
+
+    val valorUsadoOperador = listaVasia[0]
+    println(valorUsadoOperador)
+
+    val primerValor = listDeNombres.firstOrNull()
+    println(primerValor)
+
+    listaVasia.removeAt(0)
+    println(listaVasia)
+
+    listaVasia.add("Atena")
+    listaVasia.add( "Hades")
+    listaVasia.add( "Zeu")
+    listaVasia.removeIf { caracteres -> caracteres.length > 4 }
+    println(listaVasia)
+
+    val myArray = arrayOf(1, 2, 3, 4, 5)
+    println("Nuestro array $myArray")
+    println("Nuestro array como lista ${myArray.toList()}")
 }
