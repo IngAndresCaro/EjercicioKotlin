e58d44ac5ebf4de75603531d4082aaaffd30cef6
andresCaro <andrescaro189@gmail.com>
2025-02-28 19:02:17 -0500

    se entiende la variable elvis operator como un manejo de error el cual asigna un valor cuando algo llega nulo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,8 @@
 package org.example
 
 fun main() {
-
     var nombre: String? = null
-
-    try {
-        nombre?.length  // No forzar una excepción
-        nombre!!.length // Forzar una excepción esto saca el error
-
-    } catch (e: NullPointerException) {
-        println("Error")
-    }finally {
-        println("Continua sin importar que")
-    }
+    val caracteresDeNombre: Int = nombre?.length ?: 0 // Elvis operator
+    println(caracteresDeNombre)
 
 }
