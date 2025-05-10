cd7cec308eac77dd0f96048bf1c7cf615c355d55
andresCaro <andrescaro189@gmail.com>
2025-05-10 10:06:19 -0500

    Se realiza algunos ejemplos de funciones

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,4 +2,16 @@ package org.example
 
 fun main() {
 
+    //Ejemplo sumando
+    fun sumar(a: Int, b: Int): Int {
+        return a + b
+    }
+    //cuando una funcion tiene un solo return, se puede simplificar
+    fun sumar2(a: Int, b: Int) = a + b
+    //la funciones tambien devuelven nada
+    fun imprimir() {
+        println("Hola")
+    }
+
+
 }
